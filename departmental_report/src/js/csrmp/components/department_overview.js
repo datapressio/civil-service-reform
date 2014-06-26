fb48f2c528f4a0870316a5e1b6f1f9af6deb0b30
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-06-26 12:22:56 +0100

    fisr stab at some better styling

--- departmental_report/src/js/csrmp/components/department_overview.js
+++ departmental_report/src/js/csrmp/components/department_overview.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var slick = require("slick");
 var _ = require("underscore");
 var bindListener = require("../util/bind_listener")
+var dom = require("ampersand-dom");
 
 var DepartmentOverview = module.exports = function(report, budgetPie, ratingsHistogram) {
   this._report = report
@@ -28,7 +29,18 @@ DepartmentOverview.prototype._onChangeSelection = function(selection) {
   slick.find(".projects_count .n", this._element).innerHTML = department.projects_count();
   slick.find(".over_budget_count .n", this._element).innerHTML = department.over_budget_count();
   slick.find(".under_budget_count .n", this._element).innerHTML = department.under_budget_count();
-  slick.find(".total_variance .n", this._element).innerHTML = department.percent_variance().toFixed(0);
   slick.find(".total_budget .n", this._element).innerHTML = department.cash_budget();
   slick.find(".total_forecast .n", this._element).innerHTML = department.cash_forecast();
+
+ var varianceElement = slick.find(".total_variance", this._element);
+ slick.find(".n", varianceElement).innerHTML = Math.abs(department.percent_variance()).toFixed(0);
+ if(department.percent_variance() > 0) {
+   dom.addClass(varianceElement, "positive");
+   dom.removeClass(varianceElement, "negative");
+   dom.text(slick.find(".direction_label", varianceElement), "over")
+ } else {
+   dom.removeClass(varianceElement, "positive");
+   dom.addClass(varianceElement, "negative");
+   dom.text(slick.find(".direction_label", varianceElement), "under")
+ }
 };
