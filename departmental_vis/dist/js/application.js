cca19b09af0469f4a23f513c1726a9861b445dc1
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-07-08 09:29:33 +0100

    use solid colors for intermediate RAG ratings

--- departmental_vis/dist/js/application.js
+++ departmental_vis/dist/js/application.js
@@ -14063,15 +14063,28 @@ module.exports = function(object, listener) {
 
 
 },{}],40:[function(require,module,exports){
+var _  = require("underscore");
+var defaultPallete = "solid";
 var colors = module.exports =  {
-  "Green": "#2bab2b",
-  "Amber/Green": "url(\"#amberGreenHatch\")",
-  "Amber": "#e5ba39",
-  "Amber/Red": "url(\"#amberRedHatch\")",
-  "Red": "#e54545"
-}
+  hatched: {
+    "Green": "#2bab2b",
+    "Amber/Green": "url(\"#amberGreenHatch\")",
+    "Amber": "#e5ba39",
+    "Amber/Red": "url(\"#amberRedHatch\")",
+    "Red": "#e54545"
+  },
 
-},{}],41:[function(require,module,exports){
+  solid: {
+    "Green": "#2bab2b",
+    "Amber/Green": "#a6cc33",
+    "Amber": "#e5ba39",
+    "Amber/Red": "#d95f36",
+    "Red": "#e54545"
+  }
+}
+_.extend(colors, colors[defaultPallete]);
+
+},{"underscore":16}],41:[function(require,module,exports){
 module.exports = function(element, key) {
   var styles;
   if(window.getComputedStyle) {
--- departmental_vis/src/js/csrmp/util/colors.js
+++ departmental_vis/src/js/csrmp/util/colors.js
@@ -1,7 +1,20 @@
+var _  = require("underscore");
+var defaultPallete = "solid";
 var colors = module.exports =  {
-  "Green": "#2bab2b",
-  "Amber/Green": "url(\"#amberGreenHatch\")",
-  "Amber": "#e5ba39",
-  "Amber/Red": "url(\"#amberRedHatch\")",
-  "Red": "#e54545"
+  hatched: {
+    "Green": "#2bab2b",
+    "Amber/Green": "url(\"#amberGreenHatch\")",
+    "Amber": "#e5ba39",
+    "Amber/Red": "url(\"#amberRedHatch\")",
+    "Red": "#e54545"
+  },
+
+  solid: {
+    "Green": "#2bab2b",
+    "Amber/Green": "#a6cc33",
+    "Amber": "#e5ba39",
+    "Amber/Red": "#d95f36",
+    "Red": "#e54545"
+  }
 }
+_.extend(colors, colors[defaultPallete]);
